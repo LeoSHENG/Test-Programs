168c55c02f73601c8bb7cfcd116d2cf118219067
CoderMusgrove <codermusgrove@gmail.com>
2014-01-25 22:08:02 -0600

    Create Value.java

---
+++ Ulixava-Tower-Defense/Value.java
@@ -0,0 +1,17 @@
+package ecrosogames.towerdefense;
+
+public class Value {
+	
+	public static int groundGrass = 0;
+	public static int groundRoad = 1;
+	
+	public static int airAir = -1;
+	public static int airTurret = 0;
+	public static int airTrashcan = 1;
+	public static int airTowerLazer = 2;
+	
+	public static int mobAir = -1;
+	public static int mobGreeny = 0;
+	
+	public static int[] deathReward = {5};
+}
